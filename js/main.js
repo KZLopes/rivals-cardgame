5be66d0e1c43611be4ed5ac0a3dfa13501104449
Carlos Lopes <carlos.limalopes@outlook.com>
2023-03-01 18:04:38 -0300

    updated draw and restart function; added resolution and setcardvalue functions

--- js/main.js
+++ js/main.js
@@ -1,56 +1,93 @@
-document.querySelector('[name="newDeck"]').addEventListener('click', fetchDeck);
-document.querySelector('[name="start"]').addEventListener('click', fetchDeck)
-// document.querySelector('button').addEventListener('click', getFetch)
-// document.querySelector('button').addEventListener('click', getFetch)
-
-function fetchDeck(){
-  fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      let deck = data.deck_id;
-
-      document.querySelector('#playmat>h2').textContent = 'Consider our fight... Begun';
-      document.querySelector('section>span').textContent = `Remaining Cards ${data.remaining}`;
-
-      drawCard(deck)    
-      
-      
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
-  }
+if (!localStorage.getItem('personalDeck')) {fetchDeck()};
+const myDeck = localStorage.getItem('personalDeck');
   
-  function drawCard(deck) {
-    fetch(`https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('#p1 img').src = data.cards[0].image;
+document.querySelector('.draw').addEventListener('click', () => drawCard(myDeck));
+document.querySelector('[name="restart"]').addEventListener('click', () => restart(myDeck));
 
-      resolution();      
+function fetchDeck(){  
+  fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+  .then(res => res.json())
+  .then(data => {
+    localStorage.setItem('personalDeck', data.deck_id);
   })
   .catch(err => {
-    console.log(`error ${err}`)
+    console.log(`Error while fetching deck. error ${err}`)
   });
-
 }
 
-function clash() {
-  // bet the top 3 cards of players deck, use the fourth to resolve the conflict;
+function drawCard(deckId, num=1) {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`)
+  .then(res => res.json())
+  .then(data => {
+    // Create a specific function to update text??
+    document.querySelector('#p1 img').src = data.cards[num-1].image;    
+    document.querySelector('section>span').textContent = `Remaining Cards ${data.remaining}`; 
+    resolution(data.cards[num-1], data.cards[num-1]);      
+
+  })
+  .catch(err => {
+    console.log(`error ${err}`);
+  })
 }
 
 function resolution(card1, card2) {
-  //evaluates which player wins the round
+  card1 = setCardValue(card1);
+  card2 = setCardValue(card2);
+  // Evaluates which player wins the round
+  if (card1>card2){
+    document.querySelector('.outcome').textContent = 'Player 1 Wins';
+    // Update scoreboard when implemented
+    // Add BOTH cards to P1 pile
+  }else if (card2>card1){
+    document.querySelector('.outcome').textContent = 'Player 2 Wins';
+    // Update scoreboard when implemented
+    // Add BOTH cards to P2 pile
+  }else {
+    document.querySelector('.outcome').textContent = 'RUMBLE!';
+    rumble();
+  }
 }
 
+function rumble() {
+  drawCard(myDeck, 4)
+  // Bet the top 3 cards of players deck, use the fourth to resolve the conflict;
+}
 
-// function (){
-//    fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-       
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
+function restart(deckId) {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('section>span').textContent = `Remaining Cards ${data.remaining}`;       
+    
+    document.querySelector('#p1 img').src = './media/img/cardback.png';
+    // document.querySelector('outcome').textContent = '';
+  })
+  .catch(err => {
+    console.log(`Error while shuffling deck. error ${err}`)
+  });
+}
+
+function setCardValue (card) {
+  let val = card.value;
+  switch (val) {
+    case 'ACE':
+      val = 1;
+      return Number(val)
+      break;
+    case 'JACK':
+      val = 11;
+      return Number(val)
+      break;
+    case 'QUEEN':
+      val = 12;
+      return Number(val)
+      break;
+    case 'KING':
+      val = 13;
+      return Number(val)
+      break;
+    default:
+      return Number(val)
+      break;
+  }
+}
