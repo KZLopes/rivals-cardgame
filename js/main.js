98fa6e2a78d890f7e7e8d043b16b76cf7069d089
Carlos Lopes <carlos.limalopes@outlook.com>
2023-03-02 23:37:38 -0300

    Rivals playable!!

--- js/main.js
+++ js/main.js
@@ -1,70 +1,84 @@
-if (!localStorage.getItem('personalDeck')) {fetchDeck()};
-const myDeck = localStorage.getItem('personalDeck');
+if(!localStorage.getItem('p1Deck')) { fetchDeck('p1Deck') }
+if(!localStorage.getItem('p2Deck')) { fetchDeck('p2Deck') }
+//not waiting for the request to return
+const p1Deck = localStorage.getItem('p1Deck')
+const p2Deck = localStorage.getItem('p2Deck')
+let decks = [p1Deck, p2Deck];
+
+restart(decks);
+
+document.querySelector('.draw').addEventListener('click', () => drawCard(decks));
+document.querySelector('[name="restart"]').addEventListener('click', () => restart(decks));
+
+async function fetchDeck(storageKey){  
+    await fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem(`${storageKey}`, data.deck_id);
+    })
+    .catch(err => {
+      console.log(`Error while P1 fetching deck. error ${err}`)
+    });
+  };
+
+function drawCard(decksArr, num=1) {
+  let cardsInPlay = [];
+  decksArr.forEach((id, index) => {
+    fetch(`https://www.deckofcardsapi.com/api/deck/${id}/draw/?count=${num}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      document.querySelector(`#p${index+1} img`).src = data.cards[num-1].image;    
+      document.querySelector(`#p${index+1} span`).textContent = `Remaining Cards ${data.remaining}`;
+      cardsInPlay[index] = (data.cards[num-1]);
+    })
+    .then(() => {
+      resolution(cardsInPlay);
+    })
+    .catch(err => {
+      console.log(`error while drawing from ${id}.\n${err}`);
+    });     
+  }); 
+}
+
+// Evaluates which player wins the round
+function resolution(cardsArr) {
+  let p1Card = setCardValue(cardsArr[0]);
+  let p2Card = setCardValue(cardsArr[1]);
+
+  console.log('p1 ->' + p1Card);
+  console.log('p2 ->' + p2Card);
   
-document.querySelector('.draw').addEventListener('click', () => drawCard(myDeck));
-document.querySelector('[name="restart"]').addEventListener('click', () => restart(myDeck));
-
-function fetchDeck(){  
-  fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
-  .then(res => res.json())
-  .then(data => {
-    localStorage.setItem('personalDeck', data.deck_id);
-  })
-  .catch(err => {
-    console.log(`Error while fetching deck. error ${err}`)
-  });
-}
-
-function drawCard(deckId, num=1) {
-  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`)
-  .then(res => res.json())
-  .then(data => {
-    // Create a specific function to update text??
-    document.querySelector('#p1 img').src = data.cards[num-1].image;    
-    document.querySelector('section>span').textContent = `Remaining Cards ${data.remaining}`; 
-    resolution(data.cards[num-1], data.cards[num-1]);      
-
-  })
-  .catch(err => {
-    console.log(`error ${err}`);
-  })
-}
-
-function resolution(card1, card2) {
-  card1 = setCardValue(card1);
-  card2 = setCardValue(card2);
-  // Evaluates which player wins the round
-  if (card1>card2){
+  if (p1Card>p2Card){
     document.querySelector('.outcome').textContent = 'Player 1 Wins';
-    // Update scoreboard when implemented
-    // Add BOTH cards to P1 pile
-  }else if (card2>card1){
+    // Update scoreboard 
+  }else if (p2Card>p1Card){
     document.querySelector('.outcome').textContent = 'Player 2 Wins';
-    // Update scoreboard when implemented
-    // Add BOTH cards to P2 pile
+    // Update scoreboard 
   }else {
     document.querySelector('.outcome').textContent = 'RUMBLE!';
-    rumble();
+    // rumble();
   }
+  
 }
 
 function rumble() {
-  drawCard(myDeck, 4)
   // Bet the top 3 cards of players deck, use the fourth to resolve the conflict;
 }
 
-function restart(deckId) {
-  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
-  .then(res => res.json())
-  .then(data => {
-    document.querySelector('section>span').textContent = `Remaining Cards ${data.remaining}`;       
-    
-    document.querySelector('#p1 img').src = './media/img/cardback.png';
-    // document.querySelector('outcome').textContent = '';
+function restart(decksArr) {
+  decksArr.forEach ((id, index) => {
+    fetch(`https://www.deckofcardsapi.com/api/deck/${id}/shuffle/`)
+    .then(res => res.json())
+    .then(data => {
+      document.querySelector(`#p${index+1} img`).src = './media/img/cardback.png';
+      document.querySelector(`#p${index+1} span`).textContent = `Remaining Cards ${data.remaining}`;
+      // document.querySelector('outcome').textContent = '';
+    })
+    .catch(err => {
+      console.log(`Error in restart method. error ${err}`)
+    });
   })
-  .catch(err => {
-    console.log(`Error while shuffling deck. error ${err}`)
-  });
 }
 
 function setCardValue (card) {
